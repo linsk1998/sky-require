330319a79e69dbda9ccbe2d17c734e810020532c
linsk1998 <linsk@live.com>
2020-10-21 22:27:22 +0800

    readme

---
+++ README.md
@@ -0,0 +1,13 @@
+##A AMD module loader by polyfill
+
+### dependents
+
+* es.array.filter
+* es.array.includes
+* es.string.ends-with
+* es.string.starts-with
+* es.function.bind
+* es.map
+* es.set
+* es.promise
+* web.url
--- src/polyfill.js
+++ src/polyfill.js
@@ -4,7 +4,6 @@ import "core-js/modules/es.array.includes";
 import "core-js/modules/es.string.ends-with";
 import "core-js/modules/es.string.starts-with";
 import "core-js/modules/es.function.bind";
-import "core-js/modules/es.array.includes";
 import "core-js/modules/es.map";
 import "core-js/modules/es.set";
 import "core-js/modules/es.promise";
